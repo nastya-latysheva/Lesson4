03c68708f990e2b780e1af4570fe9c205113e10f
Nastya <latysheva.ana@mail.ru>
2022-11-16 22:45:26 +0300

    Задача 25

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
